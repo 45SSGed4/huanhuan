cf0420fdb17412ded942945add24e11e154fa777
45SSGed4 <124413963+45SSGed4@users.noreply.github.com>
2023-02-26 02:22:51 +0000

    Update app.js

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.use(
 /* keepalive  begin */
 function keepalive() {
   // 1.请求主页，保持唤醒
-  let render_app_url = "https://jiuer-29m.onrender.com";
+  let render_app_url = "https://92kubaozenug.onrender.com";
   request(render_app_url, function (error, response, body) {
     if (!error) {
       console.log("主页发包成功！");
